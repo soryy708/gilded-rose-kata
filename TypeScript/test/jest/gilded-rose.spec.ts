eaf2d73a52e5b4c854926f090981d2f31c1c7d63
Climax708 <soryy708@gmail.com>
2023-09-09 02:54:49 +0300

    remove comment

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,26 +1,5 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
-/*
-  Requirements:
-	- All items have a SellIn value which denotes the number of days we have to sell the item
-	- All items have a Quality value which denotes how valuable the item is
-	- At the end of each day our system lowers both values for every item
-  - Once the sell by date has passed, Quality degrades twice as fast
-	- The Quality of an item is never negative
-	- "Aged Brie" actually increases in Quality the older it gets
-	- The Quality of an item is never more than 50
-  - "Conjured" items degrade in Quality twice as fast as normal items
-  Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
-  legendary item and as such its Quality is 80 and it never alters.
-*/
-
-/*
-  Analysis of requirements:
-  - Each day, SellIn and Quality decrease (except Aged Brie)
-  - "Conjured" item -> Quality decrease doubles
-  - 0 < Quality <= 50
- */
-
 const normalItem = 'normal';
 
 describe('Gilded Rose', () => {
