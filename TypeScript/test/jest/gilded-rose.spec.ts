4791ce544fc1fdae9cb78b2fb5fce0f5f56fd148
Climax708 <soryy708@gmail.com>
2023-09-08 23:56:54 +0300

    add concise analysis of requirements unsure if correct, needs exploration

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -18,6 +18,21 @@ import { Item, GildedRose } from '@/gilded-rose';
   legendary item and as such its Quality is 80 and it never alters.
 */
 
+/*
+  Analysis of requirements:
+  - Each day, SellIn and Quality decrease (except Aged Brie)
+  - SellIn < 0 -> Quality decrease doubles
+  - "Conjured" item -> Quality decrease doubles
+  - "Aged Brie" increases in quality each day
+  - Legendary items (e.g. "Sulfuras") never decrases in quality
+  - 0 < Quality <= 50
+  - "Backstage passes" e.g. "Aged Brie"
+    - DaysToConcert <  0  -> Quality  = 0
+    - DaysToConcert <= 5  -> Quality += 3
+    - DaysToConcert <= 10 -> Quality += 2
+
+ */
+
 describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
