82ee671bd97d80d347b73d4621c56858db05d18d
Climax708 <soryy708@gmail.com>
2023-09-09 01:43:08 +0300

    simplify

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -26,47 +26,45 @@ const normalItem = 'normal';
 describe('Gilded Rose', () => {
   describe('When we have a non-expired item in stock', () => {
     describe('When a day passes', () => {
-      describe('When item is normal', () => {
-        describe('When quality is above 1', () => {
-          it("Should decrease the item's SellIn", () => {
-            const gildedRose = new GildedRose([new Item(normalItem, 2, 2)]);
-            const afterUpdate = gildedRose.updateQuality();
-            expect(afterUpdate[0].sellIn).toBe(1);
-          });
-
-          it("Should decrease the item's Quality", () => {
-            const gildedRose = new GildedRose([new Item(normalItem, 2, 2)]);
-            const afterUpdate = gildedRose.updateQuality();
-            expect(afterUpdate[0].quality).toBe(1);
-          });
+      describe('When quality is above 1', () => {
+        it("Should decrease the item's SellIn", () => {
+          const gildedRose = new GildedRose([new Item(normalItem, 2, 2)]);
+          const afterUpdate = gildedRose.updateQuality();
+          expect(afterUpdate[0].sellIn).toBe(1);
         });
 
-        describe('When quality is 1', () => {
-          it("Should decrease the item's SellIn", () => {
-            const gildedRose = new GildedRose([new Item(normalItem, 2, 1)]);
-            const afterUpdate = gildedRose.updateQuality();
-            expect(afterUpdate[0].sellIn).toBe(1);
-          });
+        it("Should decrease the item's Quality", () => {
+          const gildedRose = new GildedRose([new Item(normalItem, 2, 2)]);
+          const afterUpdate = gildedRose.updateQuality();
+          expect(afterUpdate[0].quality).toBe(1);
+        });
+      });
 
-          it("Should decrease the item's Quality", () => {
-            const gildedRose = new GildedRose([new Item(normalItem, 2, 1)]);
-            const afterUpdate = gildedRose.updateQuality();
-            expect(afterUpdate[0].quality).toBe(0);
-          });
+      describe('When quality is 1', () => {
+        it("Should decrease the item's SellIn", () => {
+          const gildedRose = new GildedRose([new Item(normalItem, 2, 1)]);
+          const afterUpdate = gildedRose.updateQuality();
+          expect(afterUpdate[0].sellIn).toBe(1);
         });
 
-        describe('When quality is 0', () => {
-          it("Should decrease the item's SellIn", () => {
-            const gildedRose = new GildedRose([new Item(normalItem, 2, 0)]);
-            const afterUpdate = gildedRose.updateQuality();
-            expect(afterUpdate[0].sellIn).toBe(1);
-          });
+        it("Should decrease the item's Quality", () => {
+          const gildedRose = new GildedRose([new Item(normalItem, 2, 1)]);
+          const afterUpdate = gildedRose.updateQuality();
+          expect(afterUpdate[0].quality).toBe(0);
+        });
+      });
 
-          it("Shouldn't change the item's Quality", () => {
-            const gildedRose = new GildedRose([new Item(normalItem, 2, 0)]);
-            const afterUpdate = gildedRose.updateQuality();
-            expect(afterUpdate[0].quality).toBe(0);
-          });
+      describe('When quality is 0', () => {
+        it("Should decrease the item's SellIn", () => {
+          const gildedRose = new GildedRose([new Item(normalItem, 2, 0)]);
+          const afterUpdate = gildedRose.updateQuality();
+          expect(afterUpdate[0].sellIn).toBe(1);
+        });
+
+        it("Shouldn't change the item's Quality", () => {
+          const gildedRose = new GildedRose([new Item(normalItem, 2, 0)]);
+          const afterUpdate = gildedRose.updateQuality();
+          expect(afterUpdate[0].quality).toBe(0);
         });
       });
     });
